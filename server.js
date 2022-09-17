4511f66a06adae2a51ca8221ec3f0a3056b24248
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-16 19:39:47 -0700

    Setup server.js

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 
 // Import required packages
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection')
+const exphbs = require('express-handlebars');
+const session = require('express-session');
 
+// Setting up sequelize & session storage
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Configuring session 
+const sess = {
+  cookie: {},
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
