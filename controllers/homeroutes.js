fb4af57befc9942fca889cd39465dd7e8df08031
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-16 19:33:52 -0700

    Setup homeroute structure

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,3 +1,35 @@
 
-// Importing authentication package
+// Importing required packages
+const router = require('express').Router();
+const withAuth = require('../utils/auth')
+const { User, Post } = require('../models');
 
+
+
+// Homepage route
+router.get('/', withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// View specific post
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// View login route
+router.get('/login', (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
