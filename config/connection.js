5a99b7065898bfa372aa0e5dd1d3c60b84c4cbd7
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-18 02:36:30 -0700

    Further testing with conection.js fixes

--- config/connection.js
+++ config/connection.js
@@ -1,39 +1,25 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-let sequelize
 
-if (process.env.DATABASE_URL) {
-  const Sequelize = require('sequelize');
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL,
+    {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
       }
     }
-  }
-  );
+  )
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-    });
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
 
 module.exports = sequelize;
