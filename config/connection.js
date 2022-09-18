7e2be79295258b3b673320c20b32b5d6318b6348
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-18 02:06:00 -0700

    Test fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,26 +3,22 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 let sequelize
 
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-  // the application is executed on Heroku ... use the postgres         database
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL,
-    {
-      dialect: "postgres",
-      protocol: "postgres",
-      port: 5432,
-      host: "<heroku host>",
-      logging: true //false
-    });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      port: 3306
+    }
+  );
 }
+
+module.exports = sequelize;
+
 
 module.exports = sequelize;
