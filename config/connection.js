9b772fd1bba0f9312827c1618fb77db41a5800c7
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-18 02:01:51 -0700

    attempt to fix connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+let sequelize
 
 if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
   // the application is executed on Heroku ... use the postgres         database
-  const sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL,
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL,
     {
       dialect: "postgres",
       protocol: "postgres",
@@ -13,7 +14,7 @@ if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
       logging: true //false
     });
 } else {
-  const sequelize = process.env.JAWSDB_URL
+  sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
